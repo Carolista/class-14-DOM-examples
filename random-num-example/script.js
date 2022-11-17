afcadc13de2ae767402ae94974c539740352f69d
Carolista <cjones.renaissancewoman@gmail.com>
2022-11-16 20:19:26 -0600

    made numBox visible upon click

--- random-num-example/script.js
+++ random-num-example/script.js
@@ -1,10 +1,9 @@
 window.addEventListener("load", function () {
-
   /* CREATE OBJECTS */
 
   // Generally a good idea to declare objects outside of listeners so they are in scope for everything
   // There are exceptions - like if the element isn't on the page yet because it gets added later
-  
+
   // Object needed for TODO #1
   const date = document.getElementById("date");
 
@@ -13,7 +12,6 @@ window.addEventListener("load", function () {
   const numBox = document.getElementById("num-box");
   const randomNum = document.getElementById("random-num");
 
-
   /* CODE TO RUN IMMEDIATELY */
 
   // Run any code needed to display things on page when it first loads
@@ -21,15 +19,16 @@ window.addEventListener("load", function () {
   // TODO #1 - Add to existing text using new Date object and .toLocaleDateString() method
   date.innerHTML += ` ${new Date().toLocaleDateString()}`;
 
-
   /* EVENT LISTENERS */
 
   // TODO #2 - Add a click event listener for the button to generate a random number
   // Will also need to make the div holding the number visible
   numButton.addEventListener("click", function () {
-
     // Test listener with log to console
     console.log("I clicked the button!");
+
+    // Make numBox visible
+    numBox.style.visibility = "visible";
 
     // Change innerHTML of randomNum to a number between 1-50
     let num = Math.ceil(Math.random() * 50);
@@ -44,18 +43,18 @@ window.addEventListener("load", function () {
       randomNum.style.color = "coral";
       document.body.style.backgroundColor = "coral";
     }
-  
+
     // TODO #4 - If the random number is evenly divisible by 3, add the spinning class; otherwise remove it
     if (num % 3 === 0) {
-      numButton.classList.add('spinning');
+      numButton.classList.add("spinning");
     } else {
-      numButton.classList.remove('spinning');
+      numButton.classList.remove("spinning");
     }
-  
+
     // TODO #5 - Trigger alert if number is evenly divisible by 5
     // NOTE: Use setTimeout() to deal with race condition; 50ms is plenty of time to delay
     if (num % 5 === 0) {
-      setTimeout(function() {
+      setTimeout(function () {
         alert(`
         The number ${num} is a multiple of 5!
         `);
@@ -70,5 +69,4 @@ window.addEventListener("load", function () {
   console.dir(numBox);
   console.log("randomNum", randomNum);
   console.dir(randomNum);
-  
 });
